07e379113c8dc51228170926b8e7a567b1ded8d1
Chris Baclig <chris@baclig.me>
2022-02-08 15:35:58 -0800

    Try sed

--- .bashrc
+++ .bashrc
@@ -1,6 +1,6 @@
 # ~/.bashrc: executed by bash(1) for non-login shells.
 
-DOTFILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export DOTFILE_DIR=__DOTFILE_DIR_PLACEHOLDER__
 
 for file in $DOTFILE_DIR/{docker,git}; do
 	[ -r "$file" ] && [ -f "$file" ] && source "$file";
--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 DOTFILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+SHELL_FILES=(.bashrc,.profile)
+
 # Override any existing files
-ln -fs ${DOTFILE_DIR}/.bashrc ~/.bashrc
-ln -fs ${DOTFILE_DIR}/.profile ~/.profile
+for shell_file in $SHELL_FILES; do
+  sed -i "s/__DOTFILE_DIR_PLACEHOLDER__/${DOTFILE_DIR}/" $shell_file && ln -s ${DOTFILE_DIR}/${shell_file} ~/${shell_file}
+done;
 
 # Don't override these if they already exist
-ln -s ${DOTFILE_DIR}/.gitconfig ~/.gitconfig
+FILES=(.gitconfig)
+
+for file in $FILES; do
+  [ ! -r "$file" ] && ln -s ${DOTFILE_DIR}/${file} ~/${file}
+done;
