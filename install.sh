f22860b828552e1e9f22f86cfc210a5f0c28c2f8
Chris Baclig <chris@baclig.me>
2022-02-08 15:01:25 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 export DOTFILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -s ${DOTFILE_DIR}/.bashrc ~/.bashrc
-ln -s ${DOTFILE_DIR}/.profile ~/.profile
+# Override any existing files
+ln -fs ${DOTFILE_DIR}/.bashrc ~/.bashrc
+ln -fs ${DOTFILE_DIR}/.profile ~/.profile
+
+# Don't override these if they already exist
 ln -s ${DOTFILE_DIR}/.gitconfig ~/.gitconfig
